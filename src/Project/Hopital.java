bf01a9c16728d85b9f3af4cfb90f15d6b06fa50a
Mohamed Larbi Jouini <MohamedLarbi.JOUINI@Esprit.tn>
2025-03-11 12:06:13 +0100

    4th commit for hospital branch

--- src/Project/Hopital.java
+++ src/Project/Hopital.java
@@ -1,4 +1,48 @@
 package Project;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Class1 {
+public class Hopital {
+
+    public Map<Medecin, ListPatients> medecinPatiens;
+
+    public Hopital() {
+        medecinPatiens = new HashMap();
+    }
+    public void ajouterMedecin(Medecin m) {
+        medecinPatiens.put(m, new ListPatients());
+
+    }
+    public void ajouterPatient(Medecin m, Patient p) {
+        if (medecinPatiens.containsKey(m)) {
+            medecinPatiens.get(m).ajouterPatient(p);
+
+        } else {
+            ajouterMedecin(m);
+            medecinPatiens.get(m).ajouterPatient(p);
+        }
+    }
+    public void afficherMap() {
+        medecinPatiens.keySet().stream().forEach(m -> {
+            System.out.println(m);
+            medecinPatiens.get(m).afficherPatients();
+        });
+
+    }
+    public void afficherMedecinPatiens() {
+        medecinPatiens.keySet().stream().filter(e -> e.getNom().equals("mohamed")).forEach(o -> {
+            System.out.println(o);
+            medecinPatiens.get(o).afficherPatients();
+        });
+
+    }
+    public List<String> RetournerNomPatients() {
+        return medecinPatiens.values().stream().map(l -> l.getListP())
+                .findFirst().get().stream().filter(p -> p.getNumSecuriteSociale() == 1)
+                .map(p -> p.getNom()).collect(Collectors.toList());
+    }
+
 }
--- src/Project/Medecin.java
+++ src/Project/Medecin.java
@@ -1,4 +1,56 @@
 package Project;
 
 public class Medecin {
+    private int cin;
+    private String nom;
+    private String prenom;
+    private int numOrdre;
+    public Medecin(int cin, String nom, String prenom, int numOrdre) {
+        this.cin = cin;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numOrdre = numOrdre;
+    }
+    public int getCin() {
+        return cin;
+    }
+    public void setCin(int cin) {
+        this.cin = cin;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public int getNumOrdre() {
+        return numOrdre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + this.cin;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Medecin other = (Medecin) obj;
+        if (this.cin != other.cin) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
