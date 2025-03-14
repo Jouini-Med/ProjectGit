8f56de764d059028cb84c479f8cd4c61b6013e45
arbiammar <arbiammar3@gmail.com>
2025-03-14 13:56:58 +0100

    Création de l'interface et développement de ses fonctionnalités

---
+++ src/Project/InterfacePatient.java
@@ -0,0 +1,9 @@
+package Project;
+
+public interface InterfacePatient {
+    public void ajouterPatient(Patient p);
+
+    public void supprimerPatient(Patient p);
+
+    public boolean rechercherPatient(Patient p);
+}
